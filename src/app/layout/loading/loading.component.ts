00a010e9085c8d96c7859f5b83f1dcb91f42fd05
carlosForero1 <carlos.quintero45828@ucaldas.edu.co>
2026-09-16 19:45:48 -0500

    Se realiza ajuste y subida de version de componente usuario

---
+++ src/app/layout/loading/loading.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+
+  @Input() mensaje = 'Cargando...';
+
+}
